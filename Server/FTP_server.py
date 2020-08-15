71efc4bae631faa1561f955979a46dfaac472198
sQUARys <46746212+sQUARys@users.noreply.github.com>
2020-08-15 23:08:25 +0200

    Added image differences, but it finds by default images

--- Server/FTP_server.py
+++ Server/FTP_server.py
@@ -1,18 +1,49 @@
+import argparse
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from io import BytesIO
+
+from cv2 import cv2
+import imutils
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from keras.models import load_model
+from PIL import Image, ImageDraw
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
-from PIL import Image, ImageDraw
-import tensorflow as tf
+from skimage.measure import compare_ssim
 from tensorflow import keras
-from keras.models import load_model
-import numpy as np
-from io import BytesIO
-import argparse
-from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
 model = load_model('/home/roma/Desktop/CNN/models/keras_model_with_new_dataset_15.h5')
 accuracy_file = "/home/roma/Desktop/Server/accuracy.txt"
+f_image = ""
+
+def image_difference():
+
+    imageA = cv2.imread("first.jpg")
+    imageB = cv2.imread("second.jpg")
+
+    grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+    grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+
+    (score, diff) = compare_ssim(grayA, grayB, full=True)
+    diff = (diff * 255).astype("uint8")
+
+    thresh = cv2.threshold(diff, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts) 
+
+    for c in cnts:
+	    (x, y, w, h) = cv2.boundingRect(c)
+	    cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	    cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    cv2.imshow("Original", imageA)
+    cv2.imshow("Modified", imageB)
+    cv2.waitKey(0)
+
 
 def model_predict(image):
     im = Image.open(image)
@@ -24,7 +55,7 @@ def model_predict(image):
 
 class MyHandler(FTPHandler):
     def on_login(self, username):
-        print("LOGIN")
+        print("LOGIN...")
         pass
 
     def on_file_received(self, file):
@@ -51,5 +82,5 @@ if __name__ == "__main__":
     handler.authorizer = authorizer
 
     server = FTPServer(("192.168.1.104", 21), handler)
+    image_difference()
     server.serve_forever()
-
