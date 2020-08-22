e29ad69b1affe65c8d83fe6aa888013526299e5e
sQUARys <46746212+sQUARys@users.noreply.github.com>
2020-08-22 23:13:09 +0200

    Image Difference install result image to pc and also gettimg a request in app. After we just need to show picture/

--- Server/FTP_server.py
+++ Server/FTP_server.py
@@ -14,6 +14,7 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 from skimage.measure import compare_ssim
+import skimage
 from tensorflow import keras
 import time
 
@@ -21,14 +22,18 @@ model = load_model('/home/roma/Desktop/CNN/models/keras_model_with_new_dataset_1
 accuracy_file = "/home/roma/Desktop/MelApp/Server/accuracy.txt"
 file_for_accuracy = "/home/roma/Desktop/MelApp/Server/picture.jpeg"
 file_for_im_diff = "/home/roma/Desktop/MelApp/Server/image_diff.jpeg"
+modified_image = "modified_im.jpeg"
+modifi_full_file = "/home/roma/Desktop/MelApp/Server/modified_im.jpeg"
 
 images_differences = ["original_image_diff.jpeg" , "image_diff.jpeg"]
 
-imageA = cv2.imread(images_differences[0])
-imageB = cv2.imread(images_differences[1])
+
 
 def image_difference():
     
+    imageA = cv2.imread(images_differences[0])
+    imageB = cv2.imread(images_differences[1])
+
     grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
     grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
 
@@ -43,6 +48,8 @@ def image_difference():
 	    (x, y, w, h) = cv2.boundingRect(c)
 	    cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
 	    cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    
+    return imageB
     
 
 
@@ -80,13 +87,20 @@ class MyHandler(FTPHandler):
                 os.rename(images_differences[1] , images_differences[0])
                 
             if os.path.isfile(images_differences[0]) == True and os.path.isfile(images_differences[1]) == True:
-                print("Second file is Uploaded")
-                time.sleep(5)
-                image_difference()
-                cv2.imshow("Modified", imageB)
-                cv2.waitKey(1000)
-                os.remove(images_differences[0])
-                os.remove(images_differences[1])
+                if os.path.isfile(modified_image):
+                    with open(modified_image, "r+") as modified_read_file: 
+                        modified_read_file.seek(0)
+                        
+                with open(modified_image, "w+") as modified_write_file: 
+                    print("Second file is Uploaded")
+                    modifi_im_method = image_difference()
+                    print(modifi_im_method)
+                    cv2.imwrite(modified_image , modifi_im_method)
+                    print("cv2.imwrite")
+                    os.remove(images_differences[0])
+                    os.remove(images_differences[1])
+                    modified_write_file.close()
+                
 
     
 if __name__ == "__main__":
